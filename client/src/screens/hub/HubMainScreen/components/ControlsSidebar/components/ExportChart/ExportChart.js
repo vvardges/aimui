c468c3fba9dd89eabab1660007f9bd56d5f5a49b
Vardges Vardanyan <vako3008@gmail.com>
2021-05-07 07:41:51 +0400

    add loader, remove click outside

--- client/src/screens/hub/HubMainScreen/components/ControlsSidebar/components/ExportChart/ExportChart.js
+++ client/src/screens/hub/HubMainScreen/components/ControlsSidebar/components/ExportChart/ExportChart.js
@@ -2,30 +2,34 @@ import React, { useState } from 'react';
 import html2canvas from 'html2canvas';
 import Modal from '../../../../../../../ui/components/Modal/Modal';
 import UI from '../../../../../../../ui';
+import loaderSrc from '../../../../../../../asset/loader.gif';
 
 function ExportChart () {
   const [imgSrc, setImgSrc] = useState(null);
+  const [isLoading, setIsLoading] = useState(false);
   const [isModalOpen, setIsModalOpen] = useState(false);
   const toggleModal = () => setIsModalOpen(!isModalOpen);
 
   const openModal = () => {
+    setIsModalOpen(true);
+    setIsLoading(true);
+
     html2canvas(document.getElementsByClassName('Panel')[0]).then(function(canvas) {
-      const img = canvas.toDataURL('image/png');
-      setImgSrc(img);
-      setIsModalOpen(true);
+      const imgSrc = canvas.toDataURL('image/png');
+      setImgSrc(imgSrc);
+      setIsLoading(false);
     });
   };
 
-  return(<>
+  return (<>
     <div className="ControlsSidebar__item" onClick={openModal}>
       <UI.Icon i='file_download' scale={1.7} />
     </div>
-    {isModalOpen && <Modal
-      toggle={toggleModal}
-      footer={<a href={imgSrc} download="chart.png">Export PNG</a>}
-    >
-      <img src={imgSrc} width="100%" alt=""/>
-    </Modal>}
+    {isModalOpen &&
+      <Modal toggle={toggleModal} footer={!isLoading && <a href={imgSrc} download="chart.png">Export PNG</a>}>
+        <img src={isLoading ? loaderSrc : imgSrc } width="100%" alt=""/>
+      </Modal>
+    }
   </>);
 }
 
--- client/src/ui/components/Modal/Modal.js
+++ client/src/ui/components/Modal/Modal.js
@@ -1,46 +1,20 @@
-import React, {useEffect, useRef} from 'react';
+import React from 'react';
 import ReactDOM from 'react-dom';
 import './Modal.less';
 
-function useOnClickOutside(ref, handler) {
-  useEffect(() => {
-    const listener = event => {
-      if (!ref.current || ref.current.contains(event.target)) {
-        return;
-      }
-    
-      handler(event);
-    };
-    
-    document.addEventListener('mousedown', listener);
-    document.addEventListener('touchstart', listener);
-    
-    return () => {
-      document.removeEventListener('mousedown', listener);
-      document.removeEventListener('touchstart', listener);
-    };
-  }, []);
-}
-
 function Modal({toggle, children, footer}) {
-  const ref = useRef();
-  useOnClickOutside(ref, () => toggle(false));
-
   return ReactDOM.createPortal(
-    <>
-      <div className="modal__backdrop"/>
-      <div className="modal">
-        <div className="modal__content" ref={ref}>
-          <div className="modal__body">
-            {children}
-          </div>
-          <div className="modal__footer">
-            {footer}
-          </div>
+    <div className="Modal" onClick={toggle}>
+      <div className="Modal__content" onClick={(e) => e.stopPropagation()}>
+        <div className="Modal__body">
+          {children}
+        </div>
+        <div className="Modal__footer">
+          {footer}
         </div>
       </div>
-    </>,
-    document.querySelector('body')
+    </div>,
+    document.querySelector('#root')
   );
 }
 
